50e1e9a59c56e4ad620d49f0452aba2c29b2367b
Dung <nguyenmanhdungtk@gmail.com>
2025-05-31 21:06:01 +0900

    Try auth with AspNetCore Identity

--- Program.cs
+++ Program.cs
@@ -1,18 +1,39 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using AuthC_.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddIdentityApiEndpoints<IdentityUser>()
+    .AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddAuthorization();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<UserContext>(options =>
-    options.UseInMemoryDatabase("Users"));
+builder.Services.AddDbContext<UserContext>(
+    options => options.UseInMemoryDatabase("Users"));
+builder.Services.AddDbContext<ApplicationDbContext>(
+    options => options.UseInMemoryDatabase("AppDb"));
+
+builder.Services.Configure<IdentityOptions>(options =>
+{
+    // Password settings.options.Password.RequireDigit = true;
+    options.Password.RequireLowercase = false;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireUppercase = false;
+    options.Password.RequireDigit = false;
+    options.Password.RequiredUniqueChars = 0;
+    options.Password.RequiredLength = 8;
+
+    // User settings.
+    options.User.RequireUniqueEmail = true;
+});
 
 var app = builder.Build();
+
+app.MapIdentityApi<IdentityUser>();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
